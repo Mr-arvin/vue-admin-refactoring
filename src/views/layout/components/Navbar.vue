d468b79c3c3786ac6fe194bf66406baad78969fb
Mr-arvin <arvin1234567890@foxmail.com>
2018-02-01 15:21:49 +0800

    面包屑中英文切换

--- src/views/layout/components/Navbar.vue
+++ src/views/layout/components/Navbar.vue
@@ -20,7 +20,7 @@
             </el-dropdown-item>
           </router-link>
           <el-dropdown-item divided>
-            <span @click="logout" style="display:block;">退出登录</span>
+            <span @click="logout" style="display:block;">{{$t('navbar.logOut')}}</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
