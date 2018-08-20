f04e5ac1b79ecc6f13e9c194515f797dcd9479a2
Mr-arvin <arvin1234567890@foxmail.com>
2018-08-20 15:53:00 +0800

    兼容ie11

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'babel-polyfill';
+
 import Vue from 'vue';
+
 import 'normalize.css/normalize.css';// A modern alternative to CSS resets
 
 import ElementUI from 'element-ui';
